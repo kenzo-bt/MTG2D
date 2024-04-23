e8672f603b64ee5f5a01740fc0bd773e176891df
kenzo-bt <kenzobalerdigit@gmail.com>
2024-04-24 02:46:45 +0300

    MTG2D-9: Added API dependencies for Render

--- MTG-Docs/API/testApp.py
+++ MTG-Docs/API/testApp.py
@@ -1,9 +1,13 @@
+import os
 import json
 from flask import Flask, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
+from flask_cors import CORS
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
