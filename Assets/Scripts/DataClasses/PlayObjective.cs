ee6e0ecc4cf4b389238196fb610ac32fc6e9d0d9
kenzo-bt <kenzobalerdigit@gmail.com>
2024-05-31 23:37:05 +0200

    MTG2D-28: Created data classes for new db models

---
+++ Assets/Scripts/DataClasses/PlayObjective.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayObjective
+{
+  public int id;
+  public string type;
+}
---
+++ Assets/Scripts/DataClasses/PlayObjectiveDeck.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayObjectiveDeck
+{
+  public int id;
+  public int objectiveid;
+  public int wins;
+  public string beaten;
+}
