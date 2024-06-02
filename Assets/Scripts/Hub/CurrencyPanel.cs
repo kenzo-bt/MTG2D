9dd53a28ef0fbd9cc6fb644d09a65edf67603441
kenzo-bt <kenzobalerdigit@gmail.com>
2024-06-02 13:54:40 +0200

    MTG2D-30: Currencies update gradually

--- Assets/Scripts/Hub/CurrencyPanel.cs
+++ Assets/Scripts/Hub/CurrencyPanel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -48,11 +49,51 @@ public class CurrencyPanel : MonoBehaviour
         {
           string serverJson = request.downloadHandler.text;
           PlayerCurrencies myCurrencies = JsonUtility.FromJson<PlayerCurrencies>(serverJson);
-          GemsCounterObject.GetComponent<TMP_Text>().text = "" + myCurrencies.gems;
-          CoinsCounterObject.GetComponent<TMP_Text>().text = "" + myCurrencies.coins;
-          PlayerManager.Instance.lastGemAmount = myCurrencies.gems;
-          PlayerManager.Instance.lastCoinAmount = myCurrencies.coins;
+          /*
+            GemsCounterObject.GetComponent<TMP_Text>().text = "" + myCurrencies.gems;
+            CoinsCounterObject.GetComponent<TMP_Text>().text = "" + myCurrencies.coins;
+            PlayerManager.Instance.lastGemAmount = myCurrencies.gems;
+            PlayerManager.Instance.lastCoinAmount = myCurrencies.coins;
+          */
+          StartCoroutine(currencyGradualAdjust(myCurrencies.gems, PlayerManager.Instance.lastGemAmount, GemsCounterObject.GetComponent<TMP_Text>(), "gems"));
+          StartCoroutine(currencyGradualAdjust(myCurrencies.coins, PlayerManager.Instance.lastCoinAmount, CoinsCounterObject.GetComponent<TMP_Text>(), "coins"));
         }
+      }
+    }
+
+    public IEnumerator currencyGradualAdjust(int updatedCurrency, int previousCurrency, TMP_Text textObject, string currencyType)
+    {
+      double currencyDouble = Convert.ToDouble(previousCurrency);
+      int difference = Math.Abs(updatedCurrency - previousCurrency);
+      double increment = Convert.ToDouble(difference / (1f / Time.deltaTime));
+      float totalTime = 0f;
+      if (updatedCurrency > previousCurrency)
+      {
+        while (Math.Floor(currencyDouble) < updatedCurrency)
+        {
+          currencyDouble += increment;
+          textObject.text = "" + Math.Floor(currencyDouble);
+          totalTime += Time.deltaTime;
+          yield return null;
+        }
+      }
+      else if (updatedCurrency < previousCurrency)
+      {
+        while (Math.Floor(currencyDouble) > updatedCurrency)
+        {
+          currencyDouble -= increment;
+          textObject.text = "" + Math.Floor(currencyDouble);
+          totalTime += Time.deltaTime;
+          yield return null;
+        }
+      }
+      if (currencyType == "gems")
+      {
+        PlayerManager.Instance.lastGemAmount = updatedCurrency;
+      }
+      else
+      {
+        PlayerManager.Instance.lastCoinAmount = updatedCurrency;
       }
     }
 }
